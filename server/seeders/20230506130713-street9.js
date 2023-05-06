0943b1b4ac4cb996c85415b969471e0328088949
MateuszRumin <rumin880@gmail.com>
2023-05-06 16:13:12 +0200

    Database_fill_streets_stops

---
+++ server/seeders/20230506130713-street9.js
@@ -0,0 +1,27 @@
+'use strict';
+const { Streets } = require('../models');
+
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const data = {
+      name:'Krótka'
+    }    
+
+    
+    const use = await Streets.findOne({ where: data });
+    if (!use){
+      const defaultValues = {
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    return queryInterface.bulkInsert('Streets', [{...data,...defaultValues}]);
+  }
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('Streets', null, {});
+  }
+};
---
+++ server/seeders/20230506134116-stops3.js
@@ -0,0 +1,35 @@
+'use strict';
+const { Streets, Stops } = require('../models');
+
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+
+
+
+  async up (queryInterface, Sequelize) {
+
+    const takeId = await Streets.findOne({ where:{ name: "Mickiewicza" } });
+
+
+    const data = {
+      name:'Mickiewicza-Rynek',
+      street_id:takeId.street_id
+    }    
+
+    
+    const use = await Stops.findOne({ where: data });
+    if (!use){
+      const defaultValues = {
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    return queryInterface.bulkInsert('Stops', [{...data,...defaultValues}]);
+  }
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('Stops', null, {});
+  }
+};
