3b77e67764e856ba84d823b7598f02a01a6c749c
pawel14011 <kontaktinfo48@gmail.com>
2023-05-06 22:03:49 +0200

    display users data

--- client/src/pages/management/Admin/ComponentsUsers/SectionUsersDisplayUsers.js
+++ client/src/pages/management/Admin/ComponentsUsers/SectionUsersDisplayUsers.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import './css/SectionUsersDisplayUsers.css'
+
 class SectionUsersDisplayUsers extends Component {
+	state = {
+		usersData: [],
+	}
+
+	componentDidMount() {
+		axios
+			.post('http://localhost:3001/select/employees/all')
+			.then(response => {
+				const usersData = response.data
+				this.setState({ usersData })
+				console.log(usersData)
+			})
+			.catch(error => {
+				console.log(error)
+			})
+	}
+
 	render() {
+		const { usersData } = this.state
 		return (
 			<section className="sectionUsersDisplayUsers">
 				<div className="headerSectionDisplayUsers">
@@ -10,7 +30,7 @@ class SectionUsersDisplayUsers extends Component {
 
 				<section className="contentDisplayUsers">
 					<div className="tbl-header">
-						<table className="tableDisplayUser" cellpadding="0" cellspacing="0" border="0">
+						<table className="tableDisplayUser" cellPadding="0" cellSpacing="0" border="0">
 							<thead>
 								<tr>
 									<th>Id</th>
@@ -25,32 +45,23 @@ class SectionUsersDisplayUsers extends Component {
 						</table>
 					</div>
 					<div className="tbl-content">
-						<table className="tableDisplayUser" cellpadding="0" cellspacing="0" border="0">
+						<table className="tableDisplayUser" cellPadding="0" cellSpacing="0" border="0">
 							<tbody>
-								<tr>
-									<td>1</td>
-									<td>Krystian</td>
-									<td>Kaczmarczyk</td>
-									<td>12312345644</td>
-									<td>Kraków 34-459 390A</td>
-									<td>123456789</td>
-									<td>Kierowca</td>
-								</tr>
-
-								<tr>
-									<td>2</td>
-									<td>Adam</td>
-									<td>Kowalski</td>
-									<td>12312345644</td>
-									<td>Wadowice 34-451 36A</td>
-									<td>123456789</td>
-									<td>Zarządca</td>
-								</tr>
+								{usersData.map(user => (
+									<tr key={user.emp_no}>
+										<td>{user.emp_no}</td>
+										<td>{user.first_name}</td>
+										<td>{user.second_name}</td>
+										<td>{user.addres}</td>
+										<td>{user.pesel}</td>
+										<td>{user.tel_num}</td>
+										<td>{user.pesel}</td>
+									</tr>
+								))}
 							</tbody>
 						</table>
 					</div>
 				</section>
-				
 			</section>
 		)
 	}
