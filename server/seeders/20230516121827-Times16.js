4b5605b4a35d528598b1ae98499275f1cfdc05d6
MateuszRumin <rumin880@gmail.com>
2023-05-16 14:40:09 +0200

    Times fill

---
+++ server/seeders/20230516121827-Times16.js
@@ -0,0 +1,48 @@
+'use strict';
+const { Stops,Times } = require('../models');
+const {Op}= require('sequelize')
+
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+
+
+
+  async up (queryInterface, Sequelize) {
+  
+    const takeId1 = await Stops.findOne({ where:{ name: "Chopina Plac Wolności" } });
+    const takeId2 = await Stops.findOne({ where:{ name: "Mickiewicza-Szpital" } });
+
+    const data = {
+      id_stop_one:takeId1.id_stop,
+      id_stop_two:takeId2.id_stop,    
+    }
+   
+    const times = {
+      Time_one_two:'00:05:00',
+      Time_two_one:'00:05:00'
+    }    
+
+    
+    const use = await Times.findOne({ where: {
+      [Op.or]:[
+        { id_stop_one:takeId1.id_stop, id_stop_two:takeId2.id_stop },
+        { id_stop_one:takeId2.id_stop, id_stop_two:takeId1.id_stop}
+      ]
+    } });
+
+    if (!use){
+      const defaultValues = {
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+
+    return queryInterface.bulkInsert('Times', [{...data,...times,...defaultValues}]);
+  }
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('Times', null, {});
+  }
+};
