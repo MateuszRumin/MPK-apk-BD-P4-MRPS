0e5396a961375c5dd6ddce8c1f5a0976fb1cf27c
MateuszRumin <rumin880@gmail.com>
2023-06-03 17:30:43 +0200

    fix pnpt.answer

--- server/controllers/data/select/routes.js
+++ server/controllers/data/select/routes.js
@@ -23,7 +23,7 @@ exports.pnpt = async (req, res) => {
         
         
         where:{id_line:req.body.id_line},
-        attributes:['id_route','order'],
+        attributes:['id_route','order','id_line','id_stop'],
         order:[['order','ASC']]
     
     }) 
