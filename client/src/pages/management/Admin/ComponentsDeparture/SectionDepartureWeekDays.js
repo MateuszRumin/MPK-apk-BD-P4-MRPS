6102d12ec65cf61ba771c4b7bbc8f4db8dddc2f7
pawel14011 <kontaktinfo48@gmail.com>
2023-06-16 18:41:50 +0200

    departure next

--- client/src/pages/management/Admin/ComponentsDeparture/SectionDepartureWeekDays.js
+++ client/src/pages/management/Admin/ComponentsDeparture/SectionDepartureWeekDays.js
@@ -6,50 +6,43 @@ import './css/SectionDepartureWeekDays.css'
 let index = 0
 let objStops = 0
 let dpd = []
+let selectTimeData;
+let weekDays = []
 const SectionDepartureWeekDays = ({ selectLine2, onChange }) => {
 	const [usersData, setUsersData] = useState([])
 	const [isOpen, setIsOpen] = useState(false)
 	const [confirmDelete, setConfirmDelete] = useState('')
 	const [setData, setSetData] = useState([])
 	const [serverResponse, setServerResponse] = useState('')
-	const [weekDays, setWeekDays] = useState([])
-	const [dest, setDest] = useState([])
 	
-let direction = true
+	const [dest, setDest] = useState([])
 
+	let direction = true
 
-const DispDept = ( object ) => {
+	const DispDept = object => {
+		let sendObj = {
+			day: 'week',
+			id_line: object.id_line,
+			id_stop: object.id_stop,
+			id_route: object.id_route,
+			direction: direction,
+		}
 
-			
-			let sendObj = {
-				day: 'week',
-				id_line: object.id_line,
-				id_stop:object.id_stop,
-				id_route: object.id_route,
-				direction: direction,
-			}
+		axios
+			// .post('http://localhost:3001/select/departure/onstop', objStops)
+			.post('http://localhost:3001/select/departure/onstop', sendObj)
+			.then(response => {
+				//  setDest[0](response.data)
 
-			axios
-				// .post('http://localhost:3001/select/departure/onstop', objStops)
-				.post('http://localhost:3001/select/departure/onstop', sendObj)
-				.then(response => {
-					//  setDest[0](response.data)
-
-					dpd[index]=response.data
-					console.log(response.data);	
-					console.log(index);
-					index++			
-				})
-				.catch(error => {
-					console.log(error)
-				})
-
-		
+				dpd[index] = response.data
+				console.log(response.data)
+				console.log(index)
+				index++
+			})
+			.catch(error => {
+				console.log(error)
+			})
 	}
-
-
-
-
 
 	const getStopsFreeForLine = () => {
 		if (objStops === 0) {
@@ -64,35 +57,25 @@ const DispDept = ( object ) => {
 			axios
 				.post('http://localhost:3001/select/routes/pnpt', objStopss)
 				.then(response => {
-					const weekDays = response.data
-					setWeekDays(weekDays)
-					console.log('Pobranie ulic z bazy')
-					console.log(weekDays)
-
+					weekDays = response.data
 					
-				
-					weekDays.forEach((object) =>{
-						console.log("jestem")
+					console.log('Pobranie ulic z bazy')
+					
+
+					weekDays.forEach(object => {
+						console.log('jestem')
 						DispDept(object)
-						
-						
-
-
 					})
-
-
-
-
-
+					index = 0
+					
 				})
+
 				.catch(error => {
 					console.log(error)
 				})
 		}
 	}
 
-
-	
 	useEffect(() => {
 		function onObjectChange(props) {
 			console.log(props)
@@ -103,15 +86,14 @@ const DispDept = ( object ) => {
 			onObjectChange(selectLine2)
 		}
 
-
-
 	}, [selectLine2])
 
 	const openModal = data => {
 		setIsOpen(true)
-		setConfirmDelete(data.stop.name)
-		setSetData(data)
-		console.log(setData)
+		// setConfirmDelete(data.stop.name)
+		// setSetData(data)
+		selectTimeData = data
+		console.log(data)
 	}
 
 	const closeModal = () => {
@@ -123,17 +105,26 @@ const DispDept = ( object ) => {
 	}
 
 	const handleConfirm = () => {
-		const validFormat = /^\d{2}:\d{2}$/.test(confirmDelete)
+		const validFormat = /^\d{2}:\d{2}:\d{2}$/.test(confirmDelete)
 		if (confirmDelete && validFormat) {
-			console.log(`Zmieniono nazwę`)
-			setData.rename = confirmDelete
-			axios.post('http://localhost:3001/test', setData).then(response => {
+			
+			let objdata = {
+				new_tile: confirmDelete,
+				num_passage: selectTimeData.num_passage,
+				id_line: objStops.id_line,
+
+			}
+
+			console.log(objdata);
+
+
+			axios.post('http://localhost:3001/test',  objdata).then(response => {
 				console.log(response.data)
 				setServerResponse(response.data) // Zapisz odpowiedź serwera w stanie
 			})
 		} else {
 			console.log('nie zmieniono.')
-			const res = 'Nie przesłano złe dane faza testów napisz np: 18:36'
+			const res = 'Nie przesłano złe dane faza testów napisz np: 18:36:00'
 			setServerResponse(res) // Zapisz odpowiedź serwera w stanie
 			// closeModal();
 		}
@@ -167,16 +158,16 @@ const DispDept = ( object ) => {
 			<div className="headerSectionDisplayStreets">
 				<span className="switchClass selectSwitch">Robocze</span>
 				<span className="switchClass" onClick={() => switchSection('So')}>
-					Soboty
+					Soboty 
 				</span>
 				<span className="switchClass" onClick={() => switchSection('Nd')}>
 					Niedziele i świeta
 				</span>
 			</div>
 			<section className="contentDisplayStreets ">
-				<div className="table-container ">
+				<div className="table-container">
 					<table className="table widthh" cellPadding="0" cellSpacing="0" border="0">
-						<thead>
+						<thead className="theadweekdays">
 							<tr>
 								{weekDays.map(order => (
 									<th className="etsttttt" key={order.order}>
@@ -185,22 +176,28 @@ const DispDept = ( object ) => {
 								))}
 							</tr>
 						</thead>
-						<tbody className="DispStreets tableDisplayStreets widthh">
-						
-								
-					{dpd.map(test => (	
-						
-								<th className="etsttttt" >
-						{test.map(dest => (
-							<tr>
-								{dest.time}
-										</tr>
-										
-										))}
-										</th>
-					
+						<tbody className="thtdtable">
+							{dpd.map((test, index) => (
+								<th className="thtdtable" key={index}>
+									{test.map((dest, destIndex) => {
+										if (index === 0 && destIndex < dpd[0].length) {
+											return (
+												<span className="thtdtable3 selectToOpen" onClick={() => openModal(dest)} key={destIndex}>
+													{dest.time}
+													<br />
+												</span>
+											)
+										} else {
+											return (
+												<span className="thtdtable3" key={destIndex}>
+													{dest.time}
+													<br />
+												</span>
+											)
+										}
+									})}
+								</th>
 							))}
-						
 						</tbody>
 					</table>
 				</div>
